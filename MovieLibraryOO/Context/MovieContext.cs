bdb47c0fbbc1d3fae3c1a9c5f775fbe6ea96e4ab
stevelinz <steve.linz@gmail.com>
2021-04-07 08:53:01 -0500

    first commit

---
+++ MovieLibraryOO/Context/MovieContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MovieLibraryOO.DataModels;
+
+namespace MovieLibraryOO.Context
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Genre> Genres {get;set;}
+        public DbSet<Movie> Movies {get;set;}
+        public DbSet<MovieGenre> MovieGenres {get;set;}
+        public DbSet<Occupation> Occupations {get;set;}
+        public DbSet<User> Users {get;set;}
+        public DbSet<UserMovie> UserMovies {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+                optionsBuilder.UseSqlServer("Server=.; database=movieDemo; User Id=sa; Password=sa; ");
+
+               
+
+                
+        }
+    }
+}
---
+++ MovieLibraryOO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using MovieLibraryOO.Context;
+using MovieLibraryOO.Data;
+
+namespace MovieLibraryOO
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // DEPENDENCY INJECTION
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IRepository, FileRepository>()
+             //   .AddSingleton<IContext, MovieContext>()
+                .AddSingleton<IMenu, Menu>()
+                .BuildServiceProvider();
+
+            // ** still have a dependency here **
+            // var repository = new MyNewRepository();
+            // var context = new MovieContext(repository);
+            // var menu = new Menu(repository, context);
+
+            // var menu = serviceProvider.GetService<IMenu>();
+            // var userSelection = menu.GetMainMenuSelection();
+
+            // while (menu.IsValid)
+            // {
+            //     menu.Process(userSelection);
+
+            //     userSelection = menu.GetMainMenuSelection();
+            // }
+
+            using (var db = new MovieContext())
+            {
+
+                var genres = db.Genres;
+
+
+                foreach (var genre in genres)
+                {
+                    System.Console.WriteLine($"({genre.Id}) {genre.Name}");
+                   
+                 }
+                
+                    
+            }
+
+            Console.WriteLine("\nThanks for using the Movie Library!");
+        }
+    }
+
+}
